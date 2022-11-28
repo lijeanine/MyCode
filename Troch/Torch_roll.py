c71923ff8dcdbad7a8e03803430766e7ad64195e
lijeanine <99231614+lijeanine@users.noreply.github.com>
2022-11-28 10:07:50 +0800

    Update Torch_roll.py

--- Troch/Torch_roll.py
+++ Troch/Torch_roll.py
@@ -22,24 +22,4 @@ def spatial_shift2(x):
     x[:,1:,:,c//2:c*3//4] = x[:,:w-1,:,c//2:c*3//4]
     x[:,:w-1,:,3*c//4:] = x[:,1:,:,3*c//4:]
     return x
-class SplitAttention(nn.Module):
-    def __init__(self, channel = 512, k = 3):
-        super().__init__()
-        self.channel = channel
-        self.k = k
-        self.mlp1 = nn.Linear(channel, channel, bias = False)
-        self.gelu = nn.GELU()
-        self.mlp2 = nn.Linear(channel, channel * k, bias = False)
-        self.softmax = nn.Softmax(1)
-    
-    def forward(self,x_all):
-        b, k, h, w, c = x_all.shape
-        x_all = x_all.reshape(b, k, -1, c)          #bs,k,n,c
-        a = torch.sum(torch.sum(x_all, 1), 1)       #bs,c
-        hat_a = self.mlp2(self.gelu(self.mlp1(a)))  #bs,kc
-        hat_a = hat_a.reshape(b, self.k, c)         #bs,k,c
-        bar_a = self.softmax(hat_a)                 #bs,k,c
-        attention = bar_a.unsqueeze(-2)             # #bs,k,1,c
-        out = attention * x_all                     # #bs,k,n,c
-        out = torch.sum(out, 1).reshape(b, h, w, c)
-        return out
+
