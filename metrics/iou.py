2dd36d0061e00f90106be97af129d8018cf919a3
lijeanine <99231614+lijeanine@users.noreply.github.com>
2022-12-12 09:57:03 +0800

    Update iou.py

--- metrics/iou.py
+++ metrics/iou.py
@@ -1,1 +1,13 @@
-22
+# examples are in dice.py:UNetx Validation and Testing
+
+def score_perclass(vol_output, ground_truth, num_classes):
+    dice_perclass = torch.zeros(num_classes)
+    iou_perclass = torch.zeros(num_classes)
+    for i in range(num_classes):
+        GT = (ground_truth == i).float()
+        Pred = (vol_output == i).float()
+        inter = torch.sum(torch.mul(GT, Pred))
+        union = torch.sum(GT) + torch.sum(Pred) + 0.0001
+        dice_perclass[i] = (2 * torch.div(inter, union))
+        iou_perclass[i] = torch.div(inter, union-inter)
+    return dice_perclass, iou_perclass
